d36e01f0fef5ce4129e6b5ac9dcc6a4db11b8138
hansenbg1 <hans15sd@gmail.com>
2026-02-07 05:55:53 -0500

    Decoupling html and serving

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<html> <body> <b>Hello</b>, World mate! </body> </html>"
+    print("Hello")
+    return render_template("index.html")
 
 #@app.route("/customers")
 #def hello_world():
